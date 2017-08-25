32ef0ac77f0a38c765114435f74c4c820bd46bed
Samuel_reis <samuelreis.1990@gmail.com>
2017-08-25 15:34:22 -0300

    Commit model project

---
+++ AutomaticoBackupOpenLP/AutomaticoBackupOpenLP/Model/RsMusicas.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomaticoBackupOpenLP.Model
+{
+    public class RsMusicas
+    {
+        public string data_musica { get; set; }
+        public string nome_musica { get; set; }
+        public string nome_musica_alternativa { get; set; }
+        public string nome_autor { get; set; }
+        public string letra_musica { get; set; }
+    }
+}
